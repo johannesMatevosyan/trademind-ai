0bbf44976c3852776bd4c6a4adef696dcc324ae6
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-05-26 18:00:54 +0400

    feat(app): import TradesModule in AppModule for trade management integration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { TradesModule } from './modules/trades/trades.module';
 import { TradingAccountsModule } from './modules/trading-accounts/trading-accounts.module';
 
 @Module({
@@ -10,6 +11,7 @@ import { TradingAccountsModule } from './modules/trading-accounts/trading-accoun
     PrismaModule,
     AuthModule,
     TradingAccountsModule,
+    TradesModule
   ],
   controllers: [AppController],
   providers: [AppService],

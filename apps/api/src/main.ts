d9c4178096d029588d3af58fdb3243296fc9bf9a
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-06-07 22:43:35 +0400

    feat(cors): enable CORS with specific origin and methods

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -9,6 +9,12 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
